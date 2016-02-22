aaec104b9790a808f62ddb5f199065f90ba92d04
jdavid05 <joeh.davidson@gmail.com>
2016-02-22 19:03:37 -0400

    Added initial files

---
+++ DisplayRect/ProcessRect.cs
@@ -0,0 +1,157 @@
+﻿/* Purpose: Display a rectangle of asterisks
+ * Author : Joseph Davidson
+ * Date   : February, 2016
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayRect
+{
+    class ProcessRect
+    {
+        #region Fields
+        /// <summary>
+        /// Fields
+        /// </summary>
+        private string coordinateXInput, coordinateYInput, widthInput    , heightInput    ;
+        private int    coordinateX     , coordinateY     , rectangleWidth, rectangleHeight;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Properties
+        /// </summary>
+        public int CoordinateX { get { return coordinateX;     } set { coordinateX = value;     } }
+        public int CoordinateY { get { return coordinateY;     } set { coordinateY = value;     } }
+        public int Width       { get { return rectangleWidth;  } set { rectangleWidth  = value; } }
+        public int Height      { get { return rectangleHeight; } set { rectangleHeight = value; } }
+        #endregion
+
+        #region Initial Constructor
+        /// <summary>
+        /// Initial Constructor -- all values specified.
+        /// </summary>
+        /// <param name="px">Point X</param>
+        /// <param name="py">Point Y</param>
+        /// <param name="wh">Width</param>
+        /// <param name="ht">Height</param>
+        public ProcessRect(int px, int py, int wh, int ht)
+        {
+            CoordinateY = py;
+            CoordinateX = px;
+            Width       = wh;
+            Height      = ht;
+        }
+        #endregion
+
+        #region Empty Constructor
+        /// <summary>
+        /// Empty constructor -- No values specified.
+        /// </summary>
+        public ProcessRect() : this(0, 0, 0, 0) { }
+        #endregion
+
+        #region Method - Display the rectangle
+        /// <summary>
+        /// Display the rectangle.
+        /// </summary>
+        public void DisplayRectDim()
+        {
+            // For every value in the sum of Coordinate Y:
+            for (int i = 0; i < CoordinateY; i++)
+            {
+                // Write an empty line.
+                Console.WriteLine();
+            }
+            // For every value in the height of the rectangle:
+            for (int j = 0; j < Height; j++)
+            {
+                // For every value in the sum of Coordinate X:
+                for (int k = 0; k < CoordinateX; k++)
+                {
+                    // Move the line over by one space.
+                    Console.Write(" ");
+                }
+                // For the width of the rectangle:
+                for (int l = 0; l < Width; l++)
+                {
+                    // Write an asterisk.
+                    Console.Write("*");
+                }
+                // Start a new line.
+                Console.WriteLine();
+            }
+            // Write an empty line.
+            Console.WriteLine();
+        }
+        #endregion
+
+        #region Public Method - Rectangle dimensions
+        /// <summary>
+        /// Ask the user to input the rectangle's dimensions.
+        /// </summary>
+        public void GetRectDim()
+        {
+            Console.Write("Enter the value for X      : ");
+            coordinateXInput = Console.ReadLine();
+
+            Console.Write("Enter the value for Y      : ");
+            coordinateYInput = Console.ReadLine();
+
+            Console.Write("Enter the value for width  : ");
+            widthInput = Console.ReadLine();
+
+            Console.Write("Enter the value for height : ");
+            heightInput = Console.ReadLine();
+
+            this.CalcRectDim();
+        }
+        #endregion
+
+        #region Public Method - Keep Going
+        /// <summary>
+        /// Ask the user if they want to continue.
+        /// </summary>
+        /// <returns>The boolean continue value.</returns>
+        public bool KeepGoing()
+        {
+            String sIn;
+            Console.Write("Do you want to continue (Y/N)? ");
+            sIn = Console.ReadLine();
+            // If the user enters 'Y' or 'y':
+            if ((sIn == "Y") || (sIn == "y"))
+            {
+                // Print an empty line and set boolean continue to true.
+                Console.WriteLine();
+                return true;
+            }
+            // If the user does not enter 'Y' or 'y':
+            else
+            {
+                // Set boolean continue to false.
+                return false;
+            }
+        }
+        #endregion
+
+        #region Private Method - Calculate Rectangle Dimensions
+        /// <summary>
+        /// Convert the X, Y, width and height strings to integers.
+        /// </summary>
+        private void CalcRectDim()
+        {
+            // Can't set properties.
+            // Error: A property, indexer, or dynamic member access may 
+            //      not be passed as an out or ref parameter.
+            int.TryParse(widthInput,  out rectangleWidth );
+            int.TryParse(heightInput, out rectangleHeight);
+            int.TryParse(coordinateXInput, out coordinateX         );
+            int.TryParse(coordinateYInput, out coordinateY         );
+            return;
+        }
+        #endregion
+    }
+}
---
+++ DisplayRect/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Purpose: Display a rectangle of asterisks
+ * Author : Joseph Davidson
+ * Date   : February, 2016
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayRect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test the initial constructor -- All parameters specified.
+            Console.WriteLine("Test the initial constructor -- x = 10, y = 3, width = 20, height = 5");
+            ProcessRect ProcessRectangle1 = new ProcessRect(10, 3, 20, 5);
+            ProcessRectangle1.DisplayRectDim();
+
+            // Test the empty constructor -- No parameters specified (Default to zero).
+            Console.WriteLine("Test the second constructor -- no values specified");
+            ProcessRect ProcessRectangle2 = new ProcessRect();
+            ProcessRectangle2.DisplayRectDim();
+
+            // Test user input -- User specifies parameters.
+            ProcessRect ProcessRectangle = new ProcessRect();
+
+            // Set the boolean continue value to true.
+            bool bContinue = true;
+            // While boolean continue is true:
+            while (bContinue)
+            {
+                // Get the user input for x, y, width, and height and display the rectangle.
+                ProcessRectangle.GetRectDim();
+                ProcessRectangle.DisplayRectDim();
+                // Ask the user if they would like to continue -- set the boolean continue value.
+                bContinue = ProcessRectangle.KeepGoing();
+            }
+
+            // While boolean continue is false allow the user to exit the program.
+            Console.Write("Press ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
